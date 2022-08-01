660871273298b601eb32f49524e1c1622a1f62b2
ravi9409 <39340361+ravi9409@users.noreply.github.com>
2022-08-02 00:57:30 +0530

    Update prime or not.java

--- prime or not.java
+++ prime or not.java
@@ -1,22 +1,25 @@
 package com.example.majorproject;
 
 public class prac {
-	public static void main(String[] args) {
-		int a = 47;
-		int count = 2;
-		boolean flag = true;
-		for (int i = 0; i < a - 2; i++) {// 0,
-			if (a % count == 0)//
-				System.out.println("Not Prime");
-			else {
-				count++;
-			}
-		}
-		if (flag == false)
-			System.out.println("Not Prime");
-		else {
-			System.out.println("Prime");
-		}
-	}
+	 public static void main(String[] args) {
+
+        int a = 57;
+        System.out.println(primeornot(a));
+
+    }
+
+    public static String primeornot(int num) {
+        boolean bool = false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0)
+                bool = true;
+        }
+
+        if (bool == true) {
+            return "Number is not prime";
+        } else {
+            return "Number is prime";
+        }
+    }
 
 }
