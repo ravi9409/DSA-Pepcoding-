3328a7743c7d8c48650fd8696babf4e61e398c9c
ravi9409 <39340361+ravi9409@users.noreply.github.com>
2022-08-02 02:12:37 +0530

    Create prime_or_not_method_2.java

---
+++ prime_or_not_method_2.java
@@ -0,0 +1,23 @@
+public class prac {
+
+    public static void main(String[] args) {
+
+        int a = 17;
+        System.out.println(primeornot(a));
+
+    }
+
+    public static String primeornot(int num) {
+        boolean bool = false;
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if (num % i == 0)
+                bool = true;
+        }
+
+        if (bool == true) {
+            return "Number is not prime";
+        } else {
+            return "Number is prime";
+        }
+    }
+}
